6f271b315ea07d79fb6903099877e8789b106990
andriikorshenko <andriikorshenko@gmail.com>
2023-07-18 14:06:09 +0200

    Add calculate winner logic

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -29,5 +29,26 @@ export class BoardComponent implements OnInit {
       this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
     }
+
+    this.winner = this.calculateWinner();
   }
+
+  calculateWinner() {
+    const lines = [
+      [0,1,2], [3,4,5], [6,7,8], // rows
+      [0,3,6], [1,4,7], [2,5,8], // columns
+      [0,4,8], [2,4,6]           // diagonals
+    ];
+
+    for(let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i];
+      if(this.squares[a] && 
+         this.squares[a] === this.squares[b] && 
+         this.squares[a] === this.squares[c]) {
+           return this.squares[a];
+         }
+    }
+
+    return '';
+  }  
 }
