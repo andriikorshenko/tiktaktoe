c19c26ae14504b9b0794f2b00213df64bf8fb4be
andriikorshenko <andriikorshenko@gmail.com>
2023-07-19 20:42:27 +0200

    Deploy to Firebase

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -6,12 +6,9 @@ import { Component, Input } from '@angular/core';
     <button *ngIf="!value">{{ value }}</button>
     <button *ngIf="value == 'X'">{{ value }}</button>
     <button *ngIf="value == 'O'">{{ value }}</button>
-    
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent  {
-
   @Input() value!: 'X' | 'O';
-
 }
